d09784b53942ab78ba072eb023386630db91778c
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-10-09 18:22:38 -0700

    add admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
   layout "layouts/page", only: [:index, :new]
   before_action :authorize!, only: [:destroy, :update, :edit]
-  # GET /pages
-  # GET /pages.json
   def index
     # @pages = Page.all
     @page = Page.new
@@ -12,23 +10,17 @@ class PagesController < ApplicationController
     @pages = @user.pages.all
   end
 
-  # GET /pages/1
-  # GET /pages/1.json
   def show
   end
 
-  # GET /pages/new
   def new
     @page = Page.new
     @templates = Template.all
   end
 
-  # GET /pages/1/edit
   def edit
   end
 
-  # POST /pages
-  # POST /pages.json
   def create
     @page = Page.new(page_params)
     @page.user = current_user
@@ -43,8 +35,6 @@ class PagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pages/1
-  # PATCH/PUT /pages/1.json
   def update
     respond_to do |format|
       if @page.update(page_params)
@@ -57,8 +47,6 @@ class PagesController < ApplicationController
     end
   end
 
-  # DELETE /pages/1
-  # DELETE /pages/1.json
   def destroy
     @page.destroy
     respond_to do |format|
@@ -68,12 +56,10 @@ class PagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_page
       @page = Page.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
       params.require(:page).permit(:page_name, :page_body, :user_id, :template_id, :image)
     end
