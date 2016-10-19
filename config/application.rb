917ed1d30db4168a32ea931666b03ff9424d5089
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-10-18 17:45:56 -0700

    add assest prpipline

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 module Final
   class Application < Rails::Application
 
